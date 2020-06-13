e61eb9a5d9b52597e1a20a85764dc848036ab6d5
Chase Johnson <Chase.b.johnson@gmail.com>
2020-06-13 15:40:12 -0700

    Finished app JS. All tests are currently passing.

--- app.js
+++ app.js
@@ -11,26 +11,119 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+let allEmployeesInfo = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const enterEmployeeInfo = () => {
+    // asks user employee info 
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'role',
+                message: 'Select an Employee type you want to enter:',
+                choices: [
+                'Manager',
+                'Engineer',
+                'Intern'
+                ]
+            },
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is this employee\'s name?'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is this employee\'s id?'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is this employee\'s email?'
+            },
+        ])
+        // asks user common questions 
+        .then(commonAnswers => {
+            const { role } = commonAnswers;
+        switch(role) {
+            case 'Manager':
+                additionalQuestions(role, "officeNumber", "What is the Manager's office number?", commonAnswers);
+            break;
+            case 'Engineer':
+                additionalQuestions(role, "github", "What is the Engineer's Github profile username?", commonAnswers);
+            break;
+            case 'Intern':
+                additionalQuestions(role, "school", "Where does the Intern go to school?", commonAnswers);
+            break;
+        }
+    });
+}
+// setup role prompt
+const additionalQuestions = (role, inputType, messageText, commonAnswers) => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: inputType,
+                message: messageText
+            }
+        ])
+        .then(answers => {
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+            let answer;
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+            for (let key in answers) {
+                answer = answers[key];
+            }
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+            const { name, id, email } = commonAnswers;
+            let employee;
+            // lets user pick role 
+            switch(role) {
+                case 'Manager':
+                    employee = new Manager(name, id, email, answer);
+                break;
+                case 'Engineer':
+                    employee = new Engineer(name, id, email, answer);
+                break;
+                case 'Intern':
+                    employee = new Intern(name, id, email, answer);
+                break;
+            }
+            allEmployeesInfo.push(employee);
+            askForAnotherEntry();
+        });
+}
+// asks user to enter an additional entry 
+const askForAnotherEntry = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'confirm',
+                name: 'new_entry',
+                message: 'Would you like to enter another employee?'
+            }
+        ])
+        .then(answer => {
+            if (answer.new_entry === true) {
+                enterEmployeeInfo();
+            }
+            else {
+                const html = render(allEmployeesInfo);
+                writeHTMLtoFile(html);
+            }
+        });
+}
+// outputs info to team.html file 
+const writeHTMLtoFile = (html) => {
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    fs.writeFile(outputPath, html, function(err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("Data successfully written to team.html file.");
+    });
+};
+
+enterEmployeeInfo();
